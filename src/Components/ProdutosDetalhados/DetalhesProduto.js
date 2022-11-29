75ab072289c3a0123d6c179d5010b241ca5375c6
Koemy <francine.pns@gmail.com>
2022-11-29 15:56:38 -0300

    Arrumei layout

--- src/Components/ProdutosDetalhados/DetalhesProduto.js
+++ src/Components/ProdutosDetalhados/DetalhesProduto.js
@@ -9,21 +9,25 @@ export default ({itens}) => {
             <div>
                 <img src="https://comeonburger.com.br/wp-content/uploads/2019/12/xbacon-especial.jpg"
                      alt="Imagem da comida" className="image"/>
-                <div className="desc">
+                <div>
                     {itens.length > 0 && itens.map((item, key) => (
-                        <div className="teste2">
-                            <div className="nome">
-                                <h2>{item.name}</h2>
-                            </div>
-                            <div className="div-preco">
-                                <div className="preco">R$29,90</div>
-                                <div className="precoMenor">R$29,90</div>
-                            </div>
-                            <div className="descricao">Pão delicioso, compra ae desgraça</div>
-                            <div className="ingredientes">Pão, bacon, hambuguer e amor</div>
-                            <div className="tags">
-                                <div style={{backgroundColor: cor2}} className="tag1">Preparando</div>
-                                <div style={{backgroundColor: cor}} className="tag2">Pronto</div>
+                        <div>
+                            <div className="desc">
+                                <div className="nome">
+                                    <h2>{item.name}</h2>
+                                </div>
+                                <div className="teste2">
+                                    <div className="div-preco">
+                                        <div className="preco">R$29,90</div>
+                                        <div className="precoMenor">R$29,90</div>
+                                    </div>
+                                    <div className="descricao">Pão delicioso, compra ae desgraça</div>
+                                    <div className="ingredientes">Pão, bacon, hambuguer e amor</div>
+                                    <div className="tags">
+                                        <div style={{backgroundColor: cor2}} className="tag1">Preparando</div>
+                                        <div style={{backgroundColor: cor}} className="tag2">Pronto</div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     ))}
